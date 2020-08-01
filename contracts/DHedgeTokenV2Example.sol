6c323227076e4876fb5ef378c4f830f8636d0a2b
Vidor Gencel <vidorge@gmail.com>
2020-08-01 15:50:30 +0200

    Refactor V2 Upgrade

--- contracts/DHedgeTokenV2Example.sol
+++ contracts/DHedgeTokenV2Example.sol
@@ -3,18 +3,20 @@ pragma solidity ^0.6.0;
 import "./DHedgeTokenV1.sol";
 
 contract DHedgeTokenV2Example is DHedgeTokenV1 {
-    uint256 public _receivedEther;
+    bool private _upgradedToV2;
+    uint256 internal _receivedEther;
 
-    function initialize() public {
-        version_ = 2;
-        require(!initialized[version_]);
-        name_ = "DHedge  DAO Token Token New Version";
-        _receivedEther = 0;
-
-        initialized[version_] = true;
+    function __DHedgeTokenV2Example_init() public {
+        require(!_upgradedToV2, "DHedgeTokenV2Example: already upgraded");
+        __DHedgeTokenV2Example_init_unchained();
     }
 
-    function brandNewFunction() public pure returns (string) {
+    function __DHedgeTokenV2Example_init_unchained() internal {
+        version = 2;
+        _receivedEther = 0;
+    }
+
+    function brandNewFunction() public pure returns (string memory) {
         return "Hello";
     }
 
