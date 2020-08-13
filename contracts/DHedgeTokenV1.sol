bbbafa1680dba3cba541f85d0c26165f5d3d4e73
Radek Ostrowski <radek@relaypay.io>
2020-08-13 16:08:45 +1000

    added dhedge logo + mit license

--- contracts/DHedgeTokenV1.sol
+++ contracts/DHedgeTokenV1.sol
@@ -3,8 +3,41 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 
+// dHedge DAO - https://dhedge.org
+//
+//      _ _   _           _
+//     ( ) ) ( )         ( )
+//    _| | |_| |  __    _| |  __    __
+//  / _  |  _  |/ __ \/ _  |/ _  \/ __ \
+// ( (_| | | | |  ___/ (_| | (_) |  ___/
+//  \__ _)_) (_)\____)\__ _)\__  |\____)
+//                         ( )_) |
+//                          \___/
+//
+// MIT License
+// ===========
+//
+// Copyright (c) 2020 dHedge
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//
 // ---------------------------------------------------------------------
-// dHedge DAO ERC20 Token - https://dhedge.org
+// dHedge DAO ERC20 Token
 //
 // Symbol         : DHT
 // Name           : dHedge DAO Token
