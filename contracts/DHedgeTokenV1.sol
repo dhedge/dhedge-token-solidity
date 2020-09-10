502d1638fa2d4904a6e1a16a401f6e60526edd12
Radek Ostrowski <radek@relaypay.io>
2020-09-10 17:59:08 +1000

    added rinkeby support, enabled optimizer, added etherscan code verification plugin, removed dhedge logo from comments

--- contracts/DHedgeTokenV1.sol
+++ contracts/DHedgeTokenV1.sol
@@ -3,21 +3,12 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 
-// dHedge DAO - https://dhedge.org
-//
-//      _ _   _           _
-//     ( ) ) ( )         ( )
-//    _| | |_| |  __    _| |  __    __
-//  / _  |  _  |/ __ \/ _  |/ _  \/ __ \
-// ( (_| | | | |  ___/ (_| | (_) |  ___/
-//  \__ _)_) (_)\____)\__ _)\__  |\____)
-//                         ( )_) |
-//                          \___/
+// dHEDGE DAO - https://dhedge.org
 //
 // MIT License
 // ===========
 //
-// Copyright (c) 2020 dHedge
+// Copyright (c) 2020 dHEDGE DAO
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
